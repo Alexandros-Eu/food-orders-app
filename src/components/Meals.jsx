4e78b7da67b7d7ba7ea1e8404f9013af756eb5a4
Alexandros-Eu <alexeouenta@gmail.com>
2025-06-14 19:17:27 +0300

    Add Meals component

---
+++ src/components/Meals.jsx
@@ -0,0 +1,12 @@
+import Meal from './Meal.jsx';
+
+export default function Meals()
+{
+    return (
+        <ol id="meals">
+            <Meal/>
+            <Meal/>
+            <Meal/>
+        </ol>
+    )
+}
