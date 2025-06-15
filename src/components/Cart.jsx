61c39d35d84894b417ba6a562bc91a2538faa325
Alexandros-Eu <alexeouenta@gmail.com>
2025-06-15 19:20:01 +0300

    Add Cart component skeleton

---
+++ src/components/Cart.jsx
@@ -0,0 +1,13 @@
+export default function Cart()
+{
+    return (
+        <dialog className="cart">
+            <h2>Your Cart</h2>
+            <ul>
+                <li>Example Item 1</li>
+                <li>Example Item 2</li>
+                <li>Example Item 3</li>
+            </ul>
+        </dialog>
+    )
+}
