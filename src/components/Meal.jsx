896c78284055a15521fe403ff387a155f0ae4137
Alexandros-Eu <alexeouenta@gmail.com>
2025-06-15 18:49:09 +0300

    Fix image src bug

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -3,7 +3,7 @@ export default function Meal({name, price, description, image})
     return (
         <li className="meal-item">
             <article>
-                <img src={image} alt="placeholder" />
+                <img src={`../backend/public/${image}`} alt="placeholder" />
                 <h3>{name}</h3>
                 <span className="meal-item-price">{price}</span>
                 <p className="meal-item-description">{description}</p>
