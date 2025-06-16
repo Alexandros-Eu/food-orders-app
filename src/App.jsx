2b22c76bf3c95d1c461abff5d8e99cfa2e37e45c
Alexandros-Eu <alexeouenta@gmail.com>
2025-06-16 23:45:24 +0300

    Add Cart modal styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header.jsx';
+import Cart from './components/Cart.jsx';
 import Meals from './components/Meals.jsx';
 
 function App() {
   return (
     <>
       <Header/>
+      <Cart/>
       <Meals/>
     </>
   );
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,67 @@
 export default function Cart()
 {
     return (
-        <dialog className="cart">
+        <dialog className="cart modal" open>
             <h2>Your Cart</h2>
             <ul>
-                <li>Example Item 1</li>
-                <li>Example Item 2</li>
-                <li>Example Item 3</li>
+                <li className="cart-item">
+                    <p>
+                        Example Item 1
+                    </p>
+                    <div className="modal-actions cart-items-action">
+                        <span className="cart-item-actions">
+                            <button>-</button>
+                        </span>
+
+                        <span>1</span>
+
+                        <span className="cart-item-actions">
+                            <button>+</button>
+                        </span>
+                    </div>
+                </li>
+
+                <li className="cart-item">
+                    <p>
+                        Example Item 2
+                    </p>
+                    <div className="modal-actions cart-items-action">
+                        <span className="cart-item-actions">
+                            <button>-</button>
+                        </span>
+
+                        <span>1</span>
+
+                        <span className="cart-item-actions">
+                            <button>+</button>
+                        </span>
+                    </div>
+                </li>
+
+                <li className="cart-item">
+                    <p>
+                        Example Item 3
+                    </p>
+                    <div className="modal-actions cart-items-action">
+                        <span className="cart-item-actions">
+                            <button>-</button>
+                        </span>
+
+                        <span>1</span>
+
+                        <span className="cart-item-actions">
+                            <button>+</button>
+                        </span>
+                    </div>
+                </li>
+
+                <span className="cart-total modal-actions">$59.99</span>
+
+                <div className="modal-actions">
+                    <button className="text-button">Close</button>
+                    <button className="button">Go to Checkout</button>
+                </div>
+
             </ul>
         </dialog>
     )
