b2daf0c1489e2e6bb868eac457a921525cb7993f
Alexandros-Eu <alexeouenta@gmail.com>
2025-06-20 02:30:10 +0300

    Add styling to Checkout component

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,31 +1,47 @@
 export default function Checkout()
 {
     return (
-        <dialog open>
+        <dialog class="modal" open>
             <form action="">
                 <h2>Checkout</h2>
                 <p>Total amount:</p>
 
-                <label htmlFor="">Full Name</label>
-                <input type="text" />
-
-                <label htmlFor="">E-mail Address</label>
-                <input type="email" />
-
-                <label htmlFor="">Street</label>
-                <input type="text" />
-
-                <label htmlFor="">Postal Code</label>
-                <input type="text" />
-
-                <div>
-                    <label htmlFor="">City</label>
-                    <input type="text" />
+                <div class="control">
+                    <label htmlFor="name">Full Name</label>
+                    <input type="text" name="name" id="name"/>
                 </div>
 
-                <div>
-                    <button>Close</button>
-                    <button>Submit Order</button>
+
+                <div class="control">
+                    <label htmlFor="email">E-mail Address</label>
+                    <input type="email" name="email" id="email"/>
+                </div>
+
+
+                <div class="control">   
+                    <label htmlFor="address">Street</label>
+                    <input type="text" name="address" id="address"/>
+                </div>
+
+
+                <div class="control-row">
+                    <div class="control">
+                        <label htmlFor="postal-code">Postal Code</label>
+                        <input type="text" name="postal-code" id="postal-code"/>
+                    </div>
+
+
+                    <div class="control">
+                        <label htmlFor="city">City</label>
+                        <input type="text" name="city" id="city"/>
+                    </div>
+
+                </div>
+   
+
+                <div class="modal-actions">
+                    <button class="text-button">Close</button>
+                    <button class="button">Submit Order</button>
                 </div>
             </form>
         </dialog>
