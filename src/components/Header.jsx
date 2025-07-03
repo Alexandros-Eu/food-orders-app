9c99f8d2134b9ff10563e2fc644f311af1779b0e
Alexandros-Eu <alexeouenta@gmail.com>
2025-07-03 22:26:50 +0300

    Add UserProgressContext + functionality of closing Cart modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,20 @@ import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button.jsx';
 import CartContext from '../store/CartContext.jsx';
+import UserProgressContext from '../store/UserProgressContext.jsx';
 
 export default function Header()
 {
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
     const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
         return totalNumberOfItems + item.quantity;
     }, 0)
+
+    function handleCartClick()
+    {
+        userProgressCtx.showCart();
+    }
 
     return (
         <header id="main-header">
@@ -17,7 +24,7 @@ export default function Header()
                 <h1>ReactFood</h1>
             </div>
             <nav>
-                <Button textOnly>Cart ({totalCartItems})</Button>
+                <Button onClick={handleCartClick} textOnly>Cart ({totalCartItems})</Button>
             </nav>
         </header>
     )
--- src/components/UI/Cart.jsx
+++ src/components/UI/Cart.jsx
@@ -1,29 +1,36 @@
 import { useContext } from 'react';
-import { Modal } from './Modal.jsx';
-import { Button } from './Button.jsx';
-import { CartContext } from '../../store/CartContext.jsx';
+import Modal from './Modal.jsx';
+import Button from './Button.jsx';
+import CartContext  from '../../store/CartContext.jsx';
+import UserProgressContext  from '../../store/UserProgressContext.jsx';
 import { currencyFormatter } from '../../util/formatting.js';
 
 
 export default function Cart()
 {
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
     const cartTotal = cartCtx.items.reduce((totalPrice, item) => {
         return  totalPrice += item.price * item.quantity
     }, 0)
 
+    function handleCartClose()
+    {
+        userProgressCtx.hideCart();
+    }
+
     return (
-        <Modal className="modal">
+        <Modal className="modal" open={userProgressCtx.progress === 'cart'}>
             <h2>Your Cart</h2>
             <ul>
-                {cartCtx.map((item) => {
+                {cartCtx.items.map((item) => {
                     return <li key={item.id}>{item.name} - {item.price}</li>
                 })}
             </ul>
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
             <p className="modal-actions">
-                <Button textOnly>Close</Button>
-                <Button>Go to Checkout</Button>
+                <Button onClick={handleCartClose} textOnly>Close</Button>
+                <Button onClick={handleCartClose}>Go to Checkout</Button>
             </p>
         </Modal>
     )
